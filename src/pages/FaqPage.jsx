b6211e9e4ace177a3e373bdca42fc3bb221739d8
Najwa Nabilah <najwa342169@gmail.com>
2025-01-11 11:24:13 +0700

    first commit

---
+++ src/pages/FaqPage.jsx
@@ -0,0 +1,11 @@
+import FaqComponents from "../components/FaqComponents";
+
+const FaqPage = () => {
+  return (
+    <div className="pt-5">
+      <FaqComponents />;
+    </div>
+  );
+};
+
+export default FaqPage;
